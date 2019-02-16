e9fd0363e22ad88f88b1fb11666501235be3e6b3
Tanmaysv <tanmaysvshetty@gmail.com>
2019-02-16 22:09:46 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/configuration/KafkaConsumerConfiguration.java
@@ -0,0 +1,40 @@
+package com.example.demo.configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfiguration {
+	
+	@Value(value = "${kafka.bootstrapAddress}")
+	private String bootstrapAddress;
+	
+	@Bean
+	public ConsumerFactory<String, String> consumerFactory(){
+		Map<String, Object> configConsumer = new HashMap<>();
+		configConsumer.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+		configConsumer.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
+		configConsumer.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		configConsumer.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		return new DefaultKafkaConsumerFactory<>(configConsumer);
+	}
+	
+	@Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(){    
+        ConcurrentKafkaListenerContainerFactory<String, String> factory=new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
+
